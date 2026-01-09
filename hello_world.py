8d5c23f3649ddb55e8b8b68c8fb4961e9a7e8d4b
Thomas Squires <thomassquires951@gmail.com>
2026-01-09 17:13:53 +0000

    Create hello_world.py module

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+"""
+Module containing hello_world function which then calls it
+"""
+
+def hello_world():
+    """
+    Print "Hello world!" to the console
+    """
+
+    print("Hello world!")
+
+hello_world()
