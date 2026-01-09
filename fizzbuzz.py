03dbeaae0e85922c945803847ea285f3482e35f1
Thomas Squires <thomassquires951@gmail.com>
2026-01-09 17:10:02 +0000

    Update fizzbuzz.py to be PEP 8 compliant

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,28 @@
-def fizzbuzz(N):
-    fizzCount = 1
-    buzzCount = 1
-    for i in range(1, N+1):
-        if i/fizzCount == 3 and i/buzzCount == 5:
+"""
+Module which defines a fizzbuzz function and then calls it
+"""
+
+def fizzbuzz(n):
+    """
+    Print every number from 1 to n, except when the number is a multiple of 3, 5 or both
+    in which case print "fizz", "buzz", and "fizzbuzz" respectively
+    
+    Arg:
+        n: The number to count to
+    """
+    fizz_count = 1
+    buzz_count = 1
+    for i in range(1, n+1):
+        if i/fizz_count == 3 and i/buzz_count == 5:
             print("fizzbuzz")
-            fizzCount += 1
-            buzzCount += 1
-        elif i/fizzCount == 3:
+            fizz_count += 1
+            buzz_count += 1
+        elif i/fizz_count == 3:
             print("fizz")
-            fizzCount += 1
-        elif i/buzzCount == 5:
+            fizz_count += 1
+        elif i/buzz_count == 5:
             print("buzz")
-            buzzCount += 1
+            buzz_count += 1
         else:
             print(i)
 
