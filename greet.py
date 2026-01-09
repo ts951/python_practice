846693ec547861385d00d99a527000f8e0b79db7
Thomas Squires <thomassquires951@gmail.com>
2026-01-09 17:28:47 +0000

    Add greet.py

---
+++ greet.py
@@ -0,0 +1,13 @@
+"""
+Contains definition of greet function and calls it
+"""
+
+def greet():
+    """
+    Ask user for name and print greeting addressing them by name
+    """
+
+    name = input("What is your name my good fellow?\n")
+    print("Ahh greetings " + name)
+
+greet()
