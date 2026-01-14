ec56972e20300aeddd6f776aca8e829aec8629f5
Thomas Squires <thomassquires951@gmail.com>
2026-01-14 22:23:57 +0000

    Make output prettier and more accurate and make script PEP 8 compliant

--- elementary/print_next_20_leap_years.py
+++ elementary/print_next_20_leap_years.py
@@ -1,12 +1,25 @@
+"""
+Prints to the terminal the next 20 leap years following the current year.
+"""
+
 import datetime
 
 year = datetime.datetime.now().year
 
-print(f"The next 20 leap years following this year ({year}) are:")
+if year%4 == 0:
+    # Ensures the listed years do not start on the current year
+    # if the current year happens to be a leap year
+    first_leap_year = year + 4
+else:
+    first_leap_year = year + year%4
 
-num_printed = 0
-while num_printed < 20:
-    year += 1
-    if year%4 == 0:
-        print(year)
-        num_printed += 1
+leap_years_str = str(first_leap_year)
+
+for i in range(1, 19):
+    leap_years_str += f", {first_leap_year + 4*i}"
+
+# 20th year is added outside of the loop so it can say "and" before it
+leap_years_str += f", and {first_leap_year + 4*19}"
+
+print(f"The next 20 leap years following this year ({year}) are:")
+print(leap_years_str)
